0a9a70bc0cf9f39b4d7d49eb4deb457dcd08e582
mishel <mishel@udm.ru>
2017-06-18 17:47:50 +0400

    Начало

---
+++ web/Web/Controllers/CommandController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class CommandController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Загрузить список команды
+        /// </summary>
+        /// <returns></returns>
+
+        public ActionResult CommandList(long id)
+        {
+            var list = new List<CommandListModel>();
+            var command = new CommandListModel();
+            command.patronymic= "Иванов";
+            command.name = "Иван";
+            command.surname = "Иванович";
+            command.position = "Защитник";
+            list.Add(command);
+             command = new CommandListModel();
+            command.patronymic = "Степанов";
+            command.name = "Степан";
+            command.surname = "Степанович";
+            command.position = "Нападающий";
+            list.Add(command);
+
+            return (new JsonResult { Data = list, JsonRequestBehavior = JsonRequestBehavior.AllowGet });
+        }
+    }
+}
---
+++ web/Web/Models/CommandListModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class CommandListModel
+    {
+        /// <summary>
+        /// ОТчество
+        /// </summary>
+        public string surname { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string patronymic { get; set; }
+
+        /// <summary>
+        /// Должность
+        /// </summary>
+        public string position { get; set; }
+        
+
+    }
+}
