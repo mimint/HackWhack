d7953e1ba4506cfb7d1416fd82d702496bf98d8b
mishel <mishel@udm.ru>
2017-06-22 07:05:07 +0400

    R

---
+++ rs/test.R
@@ -0,0 +1,15 @@
+# myfile.R
+
+#* @get /mean
+normalMean <- function(samples=10){
+  data <- rnorm(samples)
+  mean(data)
+}
+
+#* @get /sum
+addTwo <- function(a, b){
+  as.numeric(a) + as.numeric(b)
+  as.numeric(a) - as.numeric(b)
+" rfrfrfrfrfr"
+}
+	
